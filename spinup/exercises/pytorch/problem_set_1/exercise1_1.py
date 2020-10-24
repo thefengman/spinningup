1b713262b3819223199766fa8bc693f350540070
dennis <dennisfeng2@gmail.com>
2020-10-24 15:20:04 -0700

    solved exerc 1.1

--- spinup/exercises/pytorch/problem_set_1/exercise1_1.py
+++ spinup/exercises/pytorch/problem_set_1/exercise1_1.py
@@ -23,12 +23,8 @@ def gaussian_likelihood(x, mu, log_std):
     Returns:
         Tensor with shape [batch]
     """
-    #######################
-    #                     #
-    #   YOUR CODE HERE    #
-    #                     #
-    #######################
-    return torch.zeros(1)
+    return -1.0/2 * torch.sum((x-mu)**2 / torch.exp(log_std)**2 + 2 * log_std + np.log(2*np.pi),
+                              dim=1)
 
 
 if __name__ == '__main__':
