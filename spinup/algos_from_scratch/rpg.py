0f2ff786d41c5b0a845e9571bec30fa9ee7704f5
Dennis Feng <dennis@rigetti.com>
2020-11-06 18:26:06 -0800

    start of file for rpg

---
+++ spinup/algos_from_scratch/rpg.py
@@ -0,0 +1,20 @@
+"""
+This will be an implementation of the REINFORCE policy gradient algorithm (RPG), the most basic (no-frills) policy
+gradient method. Note that it will not have the improvements used by Spinning Up's VPG, which are (1) estimating V,
+for evaluating advantage, and (2) using return-to-go rather than the full return. We use the version of REINFORCE
+discussed in UC Berkeley's CS 285 (Deep RL) course.
+"""
+
+import torch
+import gym
+
+def rpg(env_fn):
+    """
+
+    TODO: identify what the output shold be (coincide with other SU algos)
+    """
+    pass
+
+
+if __name__ == '__main__':
+    pass
