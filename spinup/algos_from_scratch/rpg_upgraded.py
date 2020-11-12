29dfd1607ad2a514731143ce6afc59bc91e5da46
Dennis Feng <dennis@rigetti.com>
2020-11-11 17:50:30 -0800

    added rpg_upgraded

--- spinup/algos_from_scratch/rpg_upgraded.py
+++ spinup/algos_from_scratch/rpg_upgraded.py
@@ -1,8 +1,12 @@
 """
-This will be an implementation of the REINFORCE policy gradient algorithm (RPG), the most basic (no-frills) policy
-gradient method. Note that it will not have the improvements used by Spinning Up's VPG, which are (1) estimating V,
-for evaluating advantage, and (2) using return-to-go rather than the full return. We use the version of REINFORCE
-discussed in UC Berkeley's CS 285 (Deep RL) course.
+This will be an implementation of the an upgraded version of RPG (REINFORCE policy
+gradient), which includes 2 improvements. The 2 improvements are: (1) use rewards-to-go rather than full total
+rewards, and (2) use a reward baseline, in particular, just use the average reward of trajectories
+
+As of 2020/11/11, I have only implemented the 2nd improvment. In order to implement the 1st improvement, a couple of
+modifications would need to be done to the structure of the sums, which can be done at a future point if desired.
+For now, I've verified with avg return plots that rpg_upgraded converges faster (avg return of 500 for cartpole at
+~40 epochs) and is more stable (fewer random dips in avg return) than rpg (avg return of 500 at ~60 epochs).
 """
 
 import torch
@@ -17,7 +21,7 @@ from spinup.algos.pytorch.vpg.core import MLPCategoricalActor
 from spinup.utils.logx import EpochLogger
 
 
-def rpg(env_fn=(lambda: gym.make("CartPole-v1")), max_traj_length=500, batch_size=100, num_epochs=100,
+def rpg_upgraded(env_fn=(lambda: gym.make("CartPole-v1")), max_traj_length=500, batch_size=100, num_epochs=100,
         hidden_sizes=(32, 32), activation=nn.Tanh, pi_lr=0.0003, logger_kwargs=dict(), writer_kwargs=dict()):
     """
     Assumes env is CartPole-v1; if want to cleanup later, just make the dimensions general to any env
@@ -76,7 +80,7 @@ def rpg(env_fn=(lambda: gym.make("CartPole-v1")), max_traj_length=500, batch_siz
 
         # run one step of gradient descent optimizer
         pi_optimizer.zero_grad()
-        loss = -1 * (batch_log_prob * batch_rewards).mean()
+        loss = -1 * (batch_log_prob * (batch_rewards - batch_rewards.mean())).mean()
         loss.backward()
         pi_optimizer.step()
 
@@ -93,11 +97,11 @@ def rpg(env_fn=(lambda: gym.make("CartPole-v1")), max_traj_length=500, batch_siz
 
 if __name__ == '__main__':
     lr = 0.003
-    tag = "rpg_test"
+    tag = "rpg_upgraded_test_only_upgraded_baseline"
     logger_kwargs = {"output_dir": f"logged_data/{tag}_{lr}",
                      "exp_name": f"{tag}_{lr}"}
     writer_kwargs = {"log_dir": f"runs/{tag}_{lr}",
                      "flush_secs": 30}
 
-    rpg(batch_size=100, pi_lr=lr, logger_kwargs=logger_kwargs, writer_kwargs=writer_kwargs)
+    rpg_upgraded(batch_size=100, pi_lr=lr, logger_kwargs=logger_kwargs, writer_kwargs=writer_kwargs)
 
