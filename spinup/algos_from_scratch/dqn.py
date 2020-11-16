30dfc152d30e69386c237eda23a9027e5d89b22c
Dennis Feng <dennis@rigetti.com>
2020-11-15 22:16:21 -0800

    now I think the code of dqn() is fleshed out; next steps are to flesh out the convulational network for Q, and implement the preprocessing step too

--- spinup/algos_from_scratch/dqn.py
+++ spinup/algos_from_scratch/dqn.py
@@ -10,6 +10,7 @@ import numpy as np
 import gym
 import argparse
 import copy
+import time
 
 from spinup.utils.logx import EpochLogger
 from spinup.algos.pytorch.ddpg.core import mlp
@@ -125,72 +126,136 @@ def preprocess_obs(obs):
     return obs
 
 
-def dqn(env_fn, actor_critic=MLPActorCritic, ac_kwargs=dict(), seed=0,
-        steps_per_epoch=4000, epochs=100, replay_size=int(1e6), gamma=0.99,
-        q_lr=1e-3, batch_size=100, start_steps=10000,
-        update_after=1000, update_targ_every=50, act_noise=0.1, num_test_episodes=10,
-        max_ep_len=1000, logger_kwargs=dict(), save_freq=1, epsilon=0.0):
+def compute_loss_q(batch_data, ac, q_targ, gamma):
+    # unpack batch data; "_b" means "batch"
+    op_b, a_b, r_b, o2p_b, d_b = batch_data['obs'], batch_data['act'], batch_data['rew'], batch_data['obs2'], \
+                                 batch_data['done']
+    # assumes `a` is a 1D integer array of size batch_size
+    max_q_targ_vals = q_targ(o2p_b).max(dim=-1)  # max of Q over acts; now 1D
+    q_vals = ac.q(op_b)[np.arange(a_b.shape[0]), a_b]
+
+    loss = ((r_b + gamma * max_q_targ_vals * (1 - d_b) - q_vals) ** 2).mean()
+
+    return loss
+
+
+def run_test_episode(test_env, ac):
+    """
+    run a test episode, returning the episode return and episode length (in steps)
+    """
+    total_r = 0
+    total_steps = 0
+    o = test_env.reset()
+    op = preprocess_obs(o)
+    d = False
+    while not d:
+        a = ac.pi(torch.as_tensor(op, dtype=torch.float32))
+        o2, r, d, _ = test_env.step(a)
+        o2p = preprocess_obs(o2)
+        op = o2p
+        total_r += r
+        total_steps += 1
+    return total_r, total_steps
+
+
+def dqn(env_fn, actor_critic=MLPActorCritic, ac_kwargs=dict(), seed=0, steps_per_epoch=4000, epochs=100,
+        replay_size=int(1e6), batch_size=100, gamma=0.99, q_lr=1e-3, start_steps=10000,
+        update_after=1000, update_targ_every=50, num_test_episodes=10,
+        max_ep_len=1000, epsilon=0.0, logger_kwargs=dict(), save_freq=1):
     """
     DQN (Deep Q-Networks). Reproduce the original paper from Minh et al.
     """
+    # Instantiate env
     env = env_fn()
+    test_env = env_fn()
     # TODO: might have to assert discrete, or otherwise take only first index of shape or so
     obs_dim = env.observation_space.shape
     act_dim = env.action_space.shape
 
+    # Set up actor (pi) & critic (Q), and data buffer
     ac = actor_critic(env.observation_space, env.action_space, **ac_kwargs)
     q_targ = copy.deepcopy(ac.q)
     for p in q_targ.parameters():
         p.requires_grad = False
     q_optimizer = torch.optim.Adam(ac.q.parameters(), lr=q_lr)
 
-    # TODO where does the random seed come into play?
-
     replay_buffer = ReplayBuffer(obs_dim=obs_dim, act_dim=act_dim, size=replay_size)
 
-    def compute_loss_q(batch_data):
-        # unpack batch data; "_b" means "batch"
-        op_b, a_b, r_b, o2p_b, d_b = batch_data['obs'], batch_data['act'], batch_data['rew'], batch_data['obs2'], \
-                                     batch_data['done']
-        # assumes `a` is a 1D integer array of size batch_size
-        # assumes d is a float array (either 0.0 or 1.0)
+    # Set RNG seeds
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    env.seed(seed)
+    env.action_space.seed(seed)
 
-        max_q_targ_vals = q_targ(o2p_b).max(dim=-1)  # max of Q over acts; now 1D
-        q_vals = ac.q(op_b)[np.arange(a_b.shape[0]), a_b]
+    # Set up logging
+    logger = EpochLogger(**logger_kwargs)
+    logger.save_config(locals())
+    logger.setup_pytorch_saver(ac)
+    # TODO might want to on tensorboard as well, if want real-time logging
 
-        loss = ((r_b + gamma * max_q_targ_vals * (1 - d_b) - q_vals) ** 2).mean()
-
-        return loss
-
+    start_time = time.time()
     total_steps = epochs * steps_per_epoch
     o = env.reset()
     op = preprocess_obs(o)  # "op" = "observation_preprocessed"
+    ep_return = 0  # episode return, counter
+    ep_length = 0  # episode length, counter
     for step in range(total_steps):
         # Take an env step, then store data in replay buffer
-        a = ac.pi(torch.as_tensor(op, dtype=torch.float32))
+        if step < start_steps:
+            a = ac.pi(torch.as_tensor(op, dtype=torch.float32))
+        else:
+            a = env.action_space.sample()
         o2, r, d, _ = env.step(a)
         o2p = preprocess_obs(o2)
         replay_buffer.store(op, a, r, o2p, d)
 
+        # TODO: does DQN paper say to do 1 GD update with mean of minibatch, or many 1-data-point updates?
         # Sample a random batch from replay buffer and perform one GD step
         q_optimizer.zero_grad()
         batch_data = replay_buffer.sample_batch(batch_size)
-        loss_q = compute_loss_q(batch_data)
+        loss_q = compute_loss_q(batch_data, ac, q_targ, gamma)
         loss_q.backward()
         q_optimizer.step()
 
         # Update target network every so often
-        if step % update_targ_every == 0:
+        if (step % update_targ_every == 0) and (step >= update_after):
             q_targ = copy.deepcopy(ac.q)
             for p in q_targ.parameters():
                 p.requires_grad = False
 
-        # If episode is done, reset env
+        # Keep track of episode return and length (for logging purposes)
+        ep_return += r
+        ep_length += 1
+
+        # If episode done, reset env
         if d:
             o = env.reset()
             op = preprocess_obs(o)
+            logger.store(EpRet=ep_return, TestEpLen=ep_length)
         else:
             op = o2p
+
+        # TODO: confirm: no need for test set if test agent & env are same as training agent & env (e.g. would need
+        #  test set if algo added noise to training but not test
+        # # If epoch end, then do a test to see average return thus far
+        # if step % steps_per_epoch == steps_per_epoch - 1:
+        #     for ep_i in range(num_test_episodes):
+        #         test_ep_return, test_ep_length = run_test_episode(test_env, ac)
+        #         logger.store(TestEpRet=test_ep_return, TestEpLen=test_ep_length)
+
+        # If epoch end, save models and show logged data
+        if step % steps_per_epoch == steps_per_epoch - 1:
+            logger.save_state({'env': env}, None)  # saves both ac and env
+
+            epoch_i = int(step // steps_per_epoch)
+            logger.log_tabular("Epoch", epoch_i)
+            logger.log_tabular("EpRet", with_min_and_max=True)
+            logger.log_tabular("EpLen", average_only=True)
+            logger.log_tabular("TimeFromStart", time.time() - start_time)
+            logger.dump_tabular()
+
+    # Save model at end
+    logger.save_state({'env': env}, None)
 
 
 if __name__ == '__main__':
